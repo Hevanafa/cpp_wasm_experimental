63599e1aa1d89a821ee6301071760fc9135fc7aa
Hevanafa <dhevansafa4@gmail.com>
2025-12-11 05:56:22 +0700

    refactored main.cpp with game routine terms

--- main.cpp
+++ main.cpp
@@ -6,15 +6,33 @@ uint8_t surface[320 * 200 * 4];
 extern "C" {
   EMSCRIPTEN_KEEPALIVE  // This prevents the compiler from removing unused functions
   uint8_t* getSurfacePtr() { return surface; }
+}
 
-  EMSCRIPTEN_KEEPALIVE
-  void fillCornflowerBlue() {
-    for (int a=0; a < 320 * 200; a++) {
-      // RGBA
-      surface[a * 4] = 0x64;
-      surface[a * 4 + 1] = 0x95;
-      surface[a * 4 + 2] = 0xED;
-      surface[a * 4 + 3] = 0xFF;
-    }
+
+void fillCornflowerBlue() {
+  for (int a=0; a < 320 * 200; a++) {
+    // RGBA
+    surface[a * 4] = 0x64;
+    surface[a * 4 + 1] = 0x95;
+    surface[a * 4 + 2] = 0xED;
+    surface[a * 4 + 3] = 0xFF;
   }
+}
+
+
+void initBuffer() {
+  // TODO: initialise surface with C++'s getmem
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE
+void update() {}
+
+extern "C" EMSCRIPTEN_KEEPALIVE
+void draw() {
+  fillCornflowerBlue();
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE
+void init() {
+  initBuffer();
 }
