6e787ff5410c1419b9e50524384a97c7ff55db57
Hevanafa <dhevansafa4@gmail.com>
2025-12-11 13:13:58 +0700

    added cls

--- main.cpp
+++ main.cpp
@@ -5,11 +5,17 @@
 #define export extern "C" EMSCRIPTEN_KEEPALIVE
 #define PByte uint8_t*
 #define nil nullptr
+#define longword unsigned int
+#define word unsigned short
+#define shr >>
 // getmem --> malloc
 // freemem --> free
 
 // Begin C++
 PByte surface = nil;
+const word vgaWidth = 320;
+const word vgaHeight = 200;
+const longword bufferSize = vgaWidth * vgaHeight * 4;
 
 extern "C" {
   EMSCRIPTEN_KEEPALIVE  // This prevents the compiler from removing unused functions
@@ -19,19 +25,25 @@ extern "C" {
 }
 
 
-void fillCornflowerBlue() {
-  for (int a=0; a < 320 * 200; a++) {
+void cls(long colour) {
+  char r, g, b, a;
+  a = colour shr 24 & 0xff;
+  r = colour shr 16 & 0xff;
+  g = colour shr 8 & 0xff;
+  b = colour & 0xff;
+
+  for (int i=0; i < vgaWidth * vgaHeight; i++) {
     // RGBA
-    surface[a * 4] = 0x64;
-    surface[a * 4 + 1] = 0x95;
-    surface[a * 4 + 2] = 0xED;
-    surface[a * 4 + 3] = 0xFF;
+    surface[i * 4] = 0x64;
+    surface[i * 4 + 1] = 0x95;
+    surface[i * 4 + 2] = 0xED;
+    surface[i * 4 + 3] = 0xFF;
   }
 }
 
 
 void initBuffer() {
-  surface = (PByte)malloc(320 * 200 * 4);
+  surface = (PByte)malloc(bufferSize);
 }
 
 export void cleanup() {
@@ -44,7 +56,8 @@ export void cleanup() {
 export void update() {}
 
 export void draw() {
-  fillCornflowerBlue();
+  cls(0xFF6495ED);
+
 }
 
 export void init() {
